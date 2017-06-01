eb2c698ff2e7445c22b9d1b38cbdf932700dad33
tb3288070_88 <8271020@qq.com>
2017-06-01 22:02:18 +0800

    修改导航样式

--- src/app/sign/sign-in/sign-in.component.ts
+++ src/app/sign/sign-in/sign-in.component.ts
@@ -2,6 +2,7 @@ import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {User} from '../../shared/model/user.model';
 import {AuthService} from '../../shared/service/auth.service';
 import {Router} from '@angular/router';
+import {FormControl, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-sign-in',
@@ -11,6 +12,7 @@ import {Router} from '@angular/router';
 })
 export class SignInComponent implements OnInit {
   @Output() signedEvent = new EventEmitter<Boolean>();
+  userForm: FormControl;
   user: User;
   checkPassword: string;
   data: any;
@@ -23,14 +25,33 @@ export class SignInComponent implements OnInit {
 
   ngOnInit() {
     this.user = {};
+    this.userForm = new FormControl('');
+    /*, Validators.compose([
+     Validators.required,
+     // Validators.minLength(4),
+     ])*/
   }
 
   submit() {
+    this.signIn();
+  }
+
+  private signIn() {
     this.authService.login(this.user.name, this.user.password)
       .subscribe(data => {
         console.log(data);
         this.router.navigate(['/homepage']);
         console.log(1);
-      }, error2 => console.log(error2));
+      }, err => {
+        if (err.status === 401) {
+          alert('用户名或密码不正确!');
+          this.user = {};
+        }
+        console.log(err);
+      });
+  }
+
+  isValid(): boolean{
+    return this.userForm.valid;
   }
 }
